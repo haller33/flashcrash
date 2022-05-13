4a1ba27afdbe7aed8c8658346e19f9d283d49f78
haller33 <eliseu42lucena@gmail.com>
2022-05-12 21:31:58 -0300

    alterations for a new implementation of flashcrash

---
+++ lib/flashcrash.ex
@@ -0,0 +1,18 @@
+defmodule Flashcrash do
+  @moduledoc """
+  Documentation for `Flashcrash`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flashcrash.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flashcrash_test.exs
@@ -0,0 +1,8 @@
+defmodule FlashcrashTest do
+  use ExUnit.Case
+  doctest Flashcrash
+
+  test "greets the world" do
+    assert Flashcrash.hello() == :world
+  end
+end
